18866b0e3ef398cf087b0c37603580af87ff557d
Trevor Covington <tcovington189@gmail.com>
2020-05-23 11:12:19 -0500

    Fix special attack first click bug

--- src/components/PlayerActions.vue
+++ src/components/PlayerActions.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="d-flex justify-content-around">
+
     <button @click="attack" class="btn btn-danger" id="attack">
       <i class="fas fa-fist-raised"></i>&nbsp;Attack
     </button>
+
     <button
       @click="specialAttack"
       v-if="playerSpecials > 0"
@@ -11,12 +13,15 @@
     >
       <i class="fas fa-bolt"></i>&nbsp;Special Attack
     </button>
+
     <button @click="heal" class="btn btn-success" id="heal">
       <i class="fas fa-plus-square"></i>&nbsp;Heal
     </button>
+
     <button @click="giveUp" class="btn btn-secondary" id="give-up">
       <i class="fas fa-sad-tear"></i>&nbsp;Give Up
     </button>
+
   </div>
 </template>
 
@@ -43,8 +48,9 @@ export default {
       this.$emit('player-gives-up');
     },
     calculateDamage(min, max) {
-      const isCrit = Math.max(Math.floor(Math.random() * 10 + 1), 1) === 1;
-      const damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      const isCrit = Math.floor(Math.random() * 10 + 1) === 1;
+
+      const damage = Math.max(Math.floor(Math.random() * max + 1), min);
 
       return isCrit ? Math.ceil(max * 1.5) : damage;
     },
