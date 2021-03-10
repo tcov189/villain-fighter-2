7d45a0597005e298cf06a3f25411c42ae218cf9f
Trevor Covington <tcovington189@gmail.com>
2021-03-10 11:49:47 -0600

    deploy

---
+++ precache-manifest.137ac4cb6092e4bf1388d2dfee1a6820.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb88b1c30681670972cc",
+    "url": "/villain-fighter-2/css/app.d36f551f.css"
+  },
+  {
+    "revision": "41d244ac3c088ef0570e",
+    "url": "/villain-fighter-2/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "a17b21e8f529024b6266b8eff7280fc9",
+    "url": "/villain-fighter-2/img/characters/all-might.png"
+  },
+  {
+    "revision": "01ff5d2df81e8a6d7e58ca2ededb1bcd",
+    "url": "/villain-fighter-2/img/characters/nomu-usj.png"
+  },
+  {
+    "revision": "dc616e39a4b908bd878b18c93c1507f4",
+    "url": "/villain-fighter-2/img/characters/specials/plus-ultra.gif"
+  },
+  {
+    "revision": "ded22c202414c51fc1f98fb9512ac96c",
+    "url": "/villain-fighter-2/img/characters/specials/plus-ultra.mp4"
+  },
+  {
+    "revision": "6762c8d3cadef82a8bfbf4be84f62798",
+    "url": "/villain-fighter-2/index.html"
+  },
+  {
+    "revision": "eb88b1c30681670972cc",
+    "url": "/villain-fighter-2/js/app.83ae4df1.js"
+  },
+  {
+    "revision": "41d244ac3c088ef0570e",
+    "url": "/villain-fighter-2/js/chunk-vendors.88d70769.js"
+  },
+  {
+    "revision": "60d141e5f224ad8a8dbd9298cd5f5353",
+    "url": "/villain-fighter-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/villain-fighter-2/robots.txt"
+  }
+]);
